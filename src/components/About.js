d147852519a6d3c7a01e2ea7ccdf4d9a829de590
Andrew <a.oppman@gmail.com>
2023-08-10 02:10:21 -0400

    childrenComponents and props added to file tree

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,38 @@
 import React from "react";
 
-function About() {
+function LogBio(props) {
+  if (props.bio) {
+    return (
+      <>
+      <p>{ props.bio }</p>
+      </>
+    );
+  } else {
+    return null;
+  }
+}
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { LogBio(props) }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/" />
     </div>
   );
 }
+
+function Links(props) {
+  return (
+    <div>
+    <h3>Links</h3>
+    <a href={ props.github }>{ props.github }</a>
+    <a href={ props.linkedin }>{ props.linkedin }</a>
+    </div>
+  );
+}
+
 
 export default About;
